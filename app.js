8637fa36cc47ba289e3da3b6c1cb7d285fe2f79f
Aditya Singh <sengaraditya01@gmail.com>
2024-02-16 03:55:40 +0530

    formatted code and added some form validation

--- app.js
+++ app.js
@@ -3,113 +3,95 @@ const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
 const ejsMate = require("ejs-mate");
-app.use(express.urlencoded({extended:true}));
-const methodOverride = require('method-override');
-
 const Listing = require("./models/listing.js");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const methodOverride = require("method-override");
 
-app.set("views", path.join(__dirname, "views") );
-app.use(methodOverride('_method'));
-app.set("view engine" , "ejs");
-app.engine('ejs',ejsMate);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.engine("ejs", ejsMate);
+app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+// Function to connect to DB
 
-
-async function main(){
-    await mongoose.connect(MONGO_URL);
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
-main().then((res) => {
+//Connecting to DB
+
+main()
+  .then((res) => {
     console.log("Connected to DB");
-}).catch((err) =>  {
+  })
+  .catch((err) => {
     console.log(err);
+  });
+
+//Root Route
+
+app.get("/", (req, res) => {
+  res.send("Working");
 });
 
+//For all Listings
 
-
-app.get("/", (req,res) => {
-    res.send("Working");
+app.get("/listings", async (req, res) => {
+  allListings = await Listing.find({});
+  res.render("./listing/index.ejs", { allListings });
 });
 
-// app.get("/testListing", (req,res) => {
-//     let sampleListing = new listing({
-//         title:"my new villa",
-//         description:"This Villa is not mine",
-//         price: 1200,
-//         location:"God",
-//         country:"india"
-//     });
+//Form send for New Listing
 
-//     sampleListing.save();
-//     console.log("Sample was saved");
-//     res.send("checking Success");
-// });
-
-app.get("/listings", async (req,res) => {
-   allListings = await Listing.find({});
-   res.render("./listing/index.ejs" ,{allListings});
+app.get("/listings/new", (req, res) => {
+  res.render("./listing/new.ejs");
 });
 
-app.put("/listing/:id" , async (req,res) => {
-    let {id} = req.params;
-    let data = req.body;
-    let newListing = new Listing(data);
-    // console.log(req.body);
-    await Listing.findByIdAndUpdate(id,{...data});
-    // newListing.save().then((res) => {
-    //     console.log("edited new LIsting");
-    // }).catch((err) => {
-    //     console.log(err);
-    // });
+// Read-Show Route
 
-    res.redirect(`/listings/${id}`);
-
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("./listing/show.ejs", { listing });
 });
 
+// Edit Form Sent for Update Route
 
-app.get("/listings/new", (req,res) => {
-    res.render("./listing/new.ejs");
-    
+app.get("/listing/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("./listing/edit.ejs", { listing });
 });
 
+//Update Route in DB
 
-
-app.post("/listing", async (req,res) => {
-    let data = req.body;
-    let newListing = new Listing(data);
-    await newListing.save();
-    res.redirect("/listings");
-});
-app.get("/listings/:id", async (req,res) => {
-    let {id} = req.params;
-    let listing = await Listing.findById(id);
-    res.render("./listing/show.ejs" , {listing});
- });
-
- app.delete("/listing/:id", async (req,res) => {
-    let {id} = req.params;
-    await Listing.findOneAndDelete({_id:id});
-    res.redirect("/listings");
- });
-
- app.get("/listing/:id/edit" , async (req,res) => {
-    let {id} = req.params;
-    let listing = await Listing.findById(id);
-    res.render("./listing/edit.ejs" , {listing});
- });
-
-
-
-app.listen(3000,(res) => {
-    console.log("listening");
+app.put("/listing/:id", async (req, res) => {
+  let { id } = req.params;
+  let data = req.body;
+  let newListing = new Listing(data);
+  await Listing.findByIdAndUpdate(id, { ...data });
+  res.redirect(`/listings/${id}`);
 });
 
+//For saving New Listing to DB
 
+app.post("/listing", async (req, res) => {
+  let data = req.body;
+  let newListing = new Listing(data);
+  await newListing.save();
+  res.redirect("/listings");
+});
 
+// Delete Route
 
+app.delete("/listing/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findOneAndDelete({ _id: id });
+  res.redirect("/listings");
+});
 
-
-
-
+app.listen(8080, (res) => {
+  console.log("listening");
+});
